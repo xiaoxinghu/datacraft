be57c284d20d672244624a13ea2a65b11555893c
xiaoxing <dawnstar.hu@gmail.com>
2015-07-11 22:57:50 +1200

    use thor for cli

--- lib/doem/cli.rb
+++ lib/doem/cli.rb
@@ -2,13 +2,9 @@ require 'doem'
 
 module Doem
   # command line interface
-  class Cli
-    def self.run(args)
-      unless args.size == 1
-        puts 'Syntax: brick your-script.rb'
-        exit(-1)
-      end
-      filename = args[0]
+  class Cli < Thor
+    desc 'build [INSTRUCTION_FILE]', 'build data by instruction'
+    def build(filename)
       instruction = Instruction.from_file filename
       Doem.run instruction
     end
